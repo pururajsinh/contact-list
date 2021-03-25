7edc803acf50cdcd2c738b206d5487553ab5ea09
Pururaj Solanki <batman@Pururajs-MacBook-Pro.local>
2021-03-25 16:00:35 -0500

    Added a component to add a contact and tested JSON Server API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from "react";
 import "./App.css";
-
+import AddUser from "./components/AddUser";
 function App() {
-  return <div className="App">
-    <h1>Hello World</h1>
-  </div>;
+
+  return (
+    <div className="App">
+      <AddUser />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/AddUser.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import person from '../images/person.png';
+
+function AddUser() {
+    const [info, setInfo] = useState({});
+    const handleChange = (e) => {
+        setInfo({
+            ...info,
+            [e.target.name]: e.target.value
+        });
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await postInfo(info);
+    }
+    const postInfo = async (data) => {
+        await fetch('/info', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+    }
+    return (
+        <div className="add-user">
+            <div className="img-icon">
+                <img src={person} alt="person-icon" />
+            </div>
+            <div className="formin">
+                <form>
+                    <div className="inputvalues">
+                        <input
+                            type="text"
+                            name="firstName"
+                            placeholder="First-Name"
+                            onChange={handleChange}
+                        />
+                        <input
+                            type="text"
+                            name="lastName"
+                            placeholder="last-Name"
+                            onChange={handleChange}
+                        />
+                        <input
+                            type="text"
+                            name="contact"
+                            placeholder="Phone-Number"
+                            onChange={handleChange}
+                        />
+                        <input
+                            type="e-mail"
+                            name="email"
+                            placeholder="E-Mail"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="btn">
+                        <button onClick={handleSubmit}>Create</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default AddUser;
